ed32047bbedcb15da14f2a12ce5924ca3a4eb2fd
Tudor Pop <tudor14pop@gmail.com>
2019-05-28 18:58:14 +0300

    Solve struct

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -3,20 +3,28 @@
 
 struct ColorClassicStruct {
     // TODO: Something goes here
+    name: String,
+    hex: String,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(/* TODO: Something goes here */
+                        String, String,
+);
 
 struct ColorUnitStruct;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::string::ToString;
 
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            name: String::from("green").to_string(),
+            hex: String::from("#00FF00"),
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -26,7 +34,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
         // For more fun, use the field initialization shorthand.
-        // let green =
+        let green = ColorTupleStruct("green".to_string(), "#00FF00".to_string());
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -35,7 +43,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let green =
+         let green = ColorUnitStruct;
 
         if let ColorUnitStruct = green {
             assert!(true);
